f6c1878278eb61c4058a924bb79ee932b0ade80a
vijaykayastha0801 <51153212+vijaykayastha0801@users.noreply.github.com>
2019-05-30 17:40:46 +0530

    Server1.js

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ app.use(function(req, res, next) {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 
-app.listen(3000, '0.0.0.0', () => {
+app.listen(4000, '0.0.0.0', () => {
     console.log(`Server started on port 3000`);
 });
