f430f718f7627f5a4966c445f355b73514b6bb2e
akshay28008 <agrawa857@gmail.com>
2019-05-30 16:51:55 +0530

    Initial server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
+app.listen(3000, '0.0.0.0', () => {
+    console.log(`Server started on port 3000`);
+});
